f774cb51f13d4a09957dd30cb098c23f8d2d9f10
fakorede <abiolafakorede@gmail.com>
2020-07-29 00:57:48 +0100

    model factory and database seeder for Bookable model

---
+++ database/factories/BookableFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Bookable;
+use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
+
+$bookable = [
+    'Luxury Suites',
+    'Galaxy Villas',
+    'Premium Villas',
+    'Pristine Condos',
+    'First Class Villas',
+    'Continental Villas',
+];
+
+$factory->define(Bookable::class, function (Faker $faker) use ($bookable) {
+    return [
+        'title' => Arr::random($bookable) . ' in ' . $faker->city,
+        'description' => $faker->text()
+    ];
+});
