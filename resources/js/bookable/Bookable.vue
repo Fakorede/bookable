9f449302c8ef994b99daae4805e3a0e2975e1603
fakorede <abiolafakorede@gmail.com>
2020-07-30 02:08:26 +0100

    Availability component

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-md-8">
+      <div class="col-md-8 pb-4">
         <div class="card">
           <div class="card-body">
             <div v-if="!loading">
@@ -13,15 +13,19 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4">
-        Availability & Total Price
+      <div class="col-md-4 pb-4">
+        <availability></availability>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Availability from "./Availability";
 export default {
+  components: {
+    Availability
+  },
   data() {
     return {
       bookable: null,
