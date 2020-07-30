064e4bd19404367a61453250703bb401056a75f1
fakorede <abiolafakorede@gmail.com>
2020-07-30 23:00:24 +0100

    checking bookable availability in frontend

--- resources/js/bookable/Availability.vue
+++ resources/js/bookable/Availability.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <h6 class="text-uppercase text-secondary font-weight-bolder">Check Availability</h6>
+    <h6 class="text-uppercase text-secondary font-weight-bolder">
+      Check Availability
+      <span
+        v-if="hasAvailability"
+        class="text-success"
+      >(AVAILABLE)</span>
+      <span
+        v-if="noAvailability"
+        class="text-danger"
+      >(NOT AVAILABLE)</span>
+    </h6>
     <div class="form-row">
       <div class="form-group col-md-6">
         <label for="from">From</label>
@@ -11,7 +21,13 @@
           placeholder="Start date"
           v-model="from"
           @keyup.enter="check"
+          :class="[{'is-invalid': this.errorFor('from')}]"
         >
+        <div
+          class="invalid-feedback"
+          v-for="(error, index) in this.errorFor('from')"
+          :key="index"
+        >{{ error }}</div>
       </div>
       <div class="form-group col-md-6">
         <label for="to">To</label>
@@ -22,13 +38,20 @@
           placeholder="End date"
           v-model="to"
           @keyup.enter="check"
+          :class="[{'is-invalid': this.errorFor('to')}]"
         >
+        <div
+          class="invalid-feedback"
+          v-for="(error, index) in this.errorFor('to')"
+          :key="index"
+        >{{ error }}</div>
       </div>
     </div>
 
     <button
       class="btn btn-secondary btn-block"
       @click.prevent="check"
+      :disabled="loading"
     >Check</button>
   </div>
 </template>
@@ -38,12 +61,45 @@ export default {
   data() {
     return {
       from: null,
-      to: null
+      to: null,
+      loading: false,
+      status: null,
+      errors: null
     };
   },
   methods: {
     check() {
-      alert("clicked!");
+      this.loading = true;
+      this.errors = null;
+
+      axios
+        .get(
+          `/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+        )
+        .then(response => {
+          this.status = response.status;
+        })
+        .catch(error => {
+          if (422 === error.response.status) {
+            this.errors = error.response.data.errors;
+          }
+          this.status = error.response.status;
+        })
+        .then(() => (this.loading = false));
+    },
+    errorFor(field) {
+      return this.hasErrors && this.errors[field] ? this.errors[field] : null;
+    }
+  },
+  computed: {
+    hasErrors() {
+      return 422 === this.status && this.errors !== null;
+    },
+    hasAvailability() {
+      return 200 === this.status;
+    },
+    noAvailability() {
+      return 400 === this.status;
     }
   }
 };
