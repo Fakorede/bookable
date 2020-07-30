a1a6c3076be873d2143a3fcd586567889cfcf783
fakorede <abiolafakorede@gmail.com>
2020-07-30 03:27:36 +0100

    generated seed data for bookings

--- app/Booking.php
+++ app/Booking.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['from', 'to'];
+    
     public function bookable() 
     {
         return $this->belongsTo(Bookable::class);
