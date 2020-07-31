c250d264e5100337f2b3911b332723dc11e38dcf
fakorede <abiolafakorede@gmail.com>
2020-07-31 22:05:31 +0100

    formatting dates using momentjs and vue filters

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,11 @@ import Vue from "vue";
 import router from "./routes";
 import VueRouter from "vue-router";
 import Index from "./Index.vue";
+import moment from "moment";
 
 Vue.use(VueRouter);
+
+Vue.filter("fromNow", value => moment(value).fromNow());
 
 const app = new Vue({
     el: "#app",
