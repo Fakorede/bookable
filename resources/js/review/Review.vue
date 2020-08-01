b4d96cb6d020847b68388576a73317470c156276
fakorede <abiolafakorede@gmail.com>
2020-08-01 01:24:45 +0100

    Review page component

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label class="text-muted"
+                >Select the star rating on a scale of 1-5</label
+            >
+            <star-rating :rating="5" class="fa-3x"></star-rating>
+        </div>
+        <div class="form-group">
+            <label for="content" class="text-muted"
+                >Describe your experience</label
+            >
+            <textarea
+                name="content"
+                cols="30"
+                rows="10"
+                class="form-control"
+            ></textarea>
+        </div>
+
+        <button class="btn btn-lg btn-block btn-primary">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {}
+};
+</script>
