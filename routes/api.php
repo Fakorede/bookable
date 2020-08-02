32786754dfe5480201a8a720dac9ed38caf27918
fakorede <abiolafakorede@gmail.com>
2020-08-02 03:46:34 +0100

    storing new review functionality

--- routes/api.php
+++ routes/api.php
@@ -10,5 +10,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::apiResource('bookables', 'Api\BookableController')->only(['index', 'show']);
 Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')->name('bookables.availability');
 Route::get('bookables/{bookable}/reviews', 'Api\BookableReviewController')->name('bookables.reviews');
-Route::apiResource('reviews', 'Api\ReviewController')->only(['show']);
+Route::apiResource('reviews', 'Api\ReviewController')->only(['show', 'store']);
 Route::get('/booking-by-review/{reviewKey}', 'Api\BookingByReviewController')->name('booking.by-review');
