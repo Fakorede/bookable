6b8c35d3b0911ccbf174b101fb7867269ea8c256
fakorede <abiolafakorede@gmail.com>
2020-08-04 21:54:04 +0100

    refactor availability component to use async/await and also emit custom event

--- public/js/app.js
+++ public/js/app.js
@@ -1966,8 +1966,16 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _utils_response__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/response */ "./resources/js/utils/response.js");
-/* harmony import */ var _mixins_validationErrors__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../mixins/validationErrors */ "./resources/js/mixins/validationErrors.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/regenerator */ "./node_modules/@babel/runtime/regenerator/index.js");
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _utils_response__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../utils/response */ "./resources/js/utils/response.js");
+/* harmony import */ var _mixins_validationErrors__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../mixins/validationErrors */ "./resources/js/mixins/validationErrors.js");
+
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 //
 //
 //
@@ -2027,7 +2035,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
-  mixins: [_mixins_validationErrors__WEBPACK_IMPORTED_MODULE_1__["default"]],
+  mixins: [_mixins_validationErrors__WEBPACK_IMPORTED_MODULE_2__["default"]],
   props: {
     bookableId: {
       type: [String, Number],
@@ -2046,23 +2054,53 @@ __webpack_require__.r(__webpack_exports__);
     check: function check() {
       var _this = this;
 
-      this.loading = true;
-      this.errors = null;
-      this.$store.dispatch("setLastSearch", {
-        from: this.from,
-        to: this.to
-      });
-      axios.get("/api/bookables/".concat(this.bookableId, "/availability?from=").concat(this.from, "&to=").concat(this.to)).then(function (response) {
-        _this.status = response.status;
-      })["catch"](function (error) {
-        if (Object(_utils_response__WEBPACK_IMPORTED_MODULE_0__["is422"])(error)) {
-          _this.errors = error.response.data.errors;
-        }
+      return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _this.loading = true;
+                _this.errors = null;
 
-        _this.status = error.response.status;
-      }).then(function () {
-        return _this.loading = false;
-      });
+                _this.$store.dispatch("setLastSearch", {
+                  from: _this.from,
+                  to: _this.to
+                });
+
+                _context.prev = 3;
+                _context.next = 6;
+                return axios.get("/api/bookables/".concat(_this.bookableId, "/availability?from=").concat(_this.from, "&to=").concat(_this.to));
+
+              case 6:
+                _this.status = _context.sent.status;
+
+                _this.$emit("availability", _this.hasAvailability);
+
+                _context.next = 15;
+                break;
+
+              case 10:
+                _context.prev = 10;
+                _context.t0 = _context["catch"](3);
+
+                if (Object(_utils_response__WEBPACK_IMPORTED_MODULE_1__["is422"])(_context.t0)) {
+                  _this.errors = _context.t0.response.data.errors;
+                }
+
+                _this.status = _context.t0.response.status;
+
+                _this.$emit("availability", _this.hasAvailability);
+
+              case 15:
+                _this.loading = false;
+
+              case 16:
+              case "end":
+                return _context.stop();
+            }
+          }
+        }, _callee, null, [[3, 10]]);
+      }))();
     }
   },
   computed: {
@@ -2099,6 +2137,9 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
+//
+//
+//
 //
 //
 //
@@ -2168,6 +2209,11 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
         }
       }, _callee, null, [[1, 9]]);
     }))();
+  },
+  methods: {
+    checkPrice: function checkPrice(hasAvailability) {
+      console.log(hasAvailability);
+    }
   }
 });
 
@@ -61264,7 +61310,12 @@ var render = function() {
         { staticClass: "col-md-4 pb-4" },
         [
           _c("availability", {
-            attrs: { "bookable-id": this.$route.params.id }
+            attrs: { "bookable-id": this.$route.params.id },
+            on: {
+              availability: function($event) {
+                return _vm.checkPrice($event)
+              }
+            }
           })
         ],
         1
--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -1,24 +1,27 @@
 <template>
-  <div>
-    <div class="row">
-      <div class="col-md-8 pb-4">
-        <div class="card">
-          <div class="card-body">
-            <div v-if="!loading">
-              <h2>{{ bookable.title }}</h2>
-              <hr>
-              <article>{{ bookable.description }}</article>
+    <div>
+        <div class="row">
+            <div class="col-md-8 pb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div v-if="!loading">
+                            <h2>{{ bookable.title }}</h2>
+                            <hr />
+                            <article>{{ bookable.description }}</article>
+                        </div>
+                        <div v-else>loading data...</div>
+                    </div>
+                </div>
+                <review-list :bookable-id="this.$route.params.id"></review-list>
             </div>
-            <div v-else>loading data...</div>
-          </div>
+            <div class="col-md-4 pb-4">
+                <availability
+                    :bookable-id="this.$route.params.id"
+                    @availability="checkPrice($event)"
+                ></availability>
+            </div>
         </div>
-        <review-list :bookable-id="this.$route.params.id"></review-list>
-      </div>
-      <div class="col-md-4 pb-4">
-        <availability :bookable-id="this.$route.params.id"></availability>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -26,27 +29,32 @@ import Availability from "./Availability";
 import ReviewList from "./ReviewList";
 
 export default {
-  components: {
-    Availability,
-    ReviewList
-  },
-  data() {
-    return {
-      bookable: null,
-      loading: false
-    };
-  },
-  async created() {
-    this.loading = true;
-    try {
-      const response = await axios.get(
-        `/api/bookables/${this.$route.params.id}`
-      );
-      this.bookable = response.data.data;
-      this.loading = false;
-    } catch (err) {
-      console.log(err);
+    components: {
+        Availability,
+        ReviewList
+    },
+    data() {
+        return {
+            bookable: null,
+            loading: false
+        };
+    },
+    async created() {
+        this.loading = true;
+        try {
+            const response = await axios.get(
+                `/api/bookables/${this.$route.params.id}`
+            );
+            this.bookable = response.data.data;
+            this.loading = false;
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    methods: {
+        checkPrice(hasAvailability) {
+            console.log(hasAvailability);
+        }
     }
-  }
 };
 </script>
